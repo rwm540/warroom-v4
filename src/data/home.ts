f5d45368edfdeef679d472e7b4e8d4d0b6d954af
Hamid <159428837+rwm540@users.noreply.github.com>
2026-09-21 20:07:31 +0330

    Fix Vercel build missing data modules

---
+++ src/data/home.ts
@@ -0,0 +1,5 @@
+export const initialHomeAnnouncements = [];
+export const homeStatsData = {};
+export const faqsData = [];
+export const defaultHomeButtons = [];
+export const defaultHomeBlocks = [];
